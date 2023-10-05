27bb8c14efd5dee4de3e9c01b8c32d16f829cb6c
André Jabur <andre.jabur2@gmail.com>
2023-10-05 20:50:42 -0300

    ✨ create dense layer class

---
+++ lib/layers/dense_layer.py
@@ -0,0 +1,23 @@
+from layer import Layer
+from numpy import ndarray
+import numpy as np
+from operations import Sigmoid, WeightMultiply, BiasAdd, Operation
+
+class DenseLayer(Layer):
+    def __init__(self, num_of_neurons: int, activation: Operation) -> None:
+        super().__init__(num_of_neurons, activation)
+
+    def _setup_layer(self, input: ndarray):
+        self.params = [
+            np.random.randn(input.shape[1], self.num_of_neurons), # Pesos
+            np.random.randn(1, self.num_of_neurons)               # Bias
+        ]
+
+        self.operations = [
+            WeightMultiply(self.params[0]), 
+            BiasAdd(self.params[1]),
+            self.activation
+        ]
+
+
+    
---
+++ lib/operations/__init__.py
@@ -0,0 +1,5 @@
+from bias_add import BiasAdd
+from operation import Operation
+from param_operation import ParamOperation
+from sigmoid import Sigmoid
+from weight_multiply import WeightMultiply
