6568a57f36174700fea8138d54b94e142d2826d5
André Jabur <andre.jabur2@gmail.com>
2023-10-05 19:00:46 -0300

    ✨ add weight multiply operator

---
+++ lib/weight_multiply.py
@@ -0,0 +1,21 @@
+from param_operation import ParamOperation
+from numpy import ndarray
+import numpy as np
+
+class WeightMultiply(ParamOperation):
+    '''
+    Operação que multiplica a entrada por pesos
+    '''
+
+    def __init__(self, W: ndarray):
+        super().__init__()
+
+    def _evaluate_output(self) -> ndarray:
+        return self.input_ @ self.param
+    
+    def _evaluate_input_grad(self, output_grad: ndarray) -> ndarray:
+        return output_grad @ self.param.T
+    
+    def _evaluate_param_grad(self, output_grad: ndarray) -> ndarray:
+        return self.input_.T @ output_grad
+
